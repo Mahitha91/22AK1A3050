448831dd7b501d21aa3515bf453e641fbf6a3327
unknown <Mahitha91>
2024-09-14 12:28:16 +0530

    adding my file

---
+++ multiplymatrices.js
@@ -0,0 +1,35 @@
+function multiplyMatrices(A, B) {
+    console.log("Matrix A:");
+    console.log(A);
+    console.log("Matrix B:");
+    console.log(B);
+    let result = [];
+    for (let i = 0; i < A.length; i++) {
+        result[i] = [];
+        for (let j = 0; j < B[0].length; j++) {
+            result[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < A.length; i++) {
+        for (let j = 0; j < B[0].length; j++) {
+            for (let k = 0; k < A[0].length; k++) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    console.log("Resultant Matrix:");
+    console.log(result);
+
+    return result;
+}
+const A = [
+    [1, 2],
+    [3, 4]
+];
+const B = [
+    [5, 6],
+    [7, 8]
+];
+
+multiplyMatrices(A, B);
